9bf6346f883b63d89da52c3586f63a9ac4546cdd
Zacck Osiemo <coderv63@gmail.com>
2025-08-31 10:41:08 +0200

    neurons: Add sigmoid implementation
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- main.c
+++ main.c
@@ -1,63 +1,114 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 
-struct neuron_input {
+
+struct perceptron_input {
 	int value; 
 	int weight; 
 };
 
-int fire_percpetron(struct neuron_input *inputs, int threshold, int input_length);
-int sum_of_weights(struct neuron_input *inputs, int input_length); 
+struct sigmoid_input {
+	float value; 
+	float weight; 
+};
+
+bool fire_percpetron(struct perceptron_input *inputs, int threshold, int input_length);
+int sum_of_perceptron_weights(struct perceptron_input *inputs, int input_length);
+
+bool fire_sigmoid(struct sigmoid_input *inputs, float threshold, int input_length);
+float sum_of_sigmoid_weights(struct sigmoid_input *inputs, int input_length);
 
 int main() {
 	int ret; 
 
-	struct neuron_input cheese_input = {
+	struct perceptron_input cheese_input = {
 		.value = 1, 
 		.weight = 3, 
 	};
 
-	struct neuron_input gluten_input = {
+	struct perceptron_input gluten_input = {
 		.value = 0, 
 		.weight = 5,
 	};
 
-	struct neuron_input inputs[] = {cheese_input, gluten_input};
+	struct perceptron_input inputs[] = {cheese_input, gluten_input};
 
-	int fire  = fire_percpetron(inputs, 4, 2);
+	bool fire  = fire_percpetron(inputs, 4, 2);
 
-	if(fire < 1) {
+	if(fire) {
+		printf("There is no gluten we can go \n \n");
+	} else  {
+		printf("There is gluten we can't go \n \n");
+	}
+
+	struct sigmoid_input sigmoid_cheese = {
+		.value = 3.98, 
+		.weight = 4.0, 
+	};
+
+	struct sigmoid_input sigmoid_gluten = {
+		.value = 0.02, 
+		.weight = 4.5,
+	};
+
+	struct sigmoid_input sinputs[] = {sigmoid_cheese, sigmoid_gluten}; 
+
+	fire = fire_sigmoid(sinputs, 4.5, 2);
+
+	if(fire) {
 		printf("There is no gluten we can go \n");
 	} else  {
 		printf("There is gluten we can't go \n");
 	}
 
+
 	ret = 0;
 	return  ret;
 }
 
-int fire_percpetron(struct neuron_input *inputs, int threshold, int input_length){ 
+bool fire_sigmoid(struct sigmoid_input *inputs, float threshold, int input_length) {
+	for(int i = 0; i < input_length; i++) {
+		printf("input value: %.2f, weight: %.2f \n", inputs[i].value, inputs[i].weight);
+	}
+
+	float ret; 
+	ret = sum_of_sigmoid_weights(inputs, input_length); 
+
+	printf("sum of weights is: %.2f \n", ret);
+	printf("Threshhold is: %.2f \n \n" , threshold);
+
+	ret = 1 / (1 + exp(-(ret - threshold)));
+
+	return ret <= 0.5;
+}
+
+float sum_of_sigmoid_weights(struct sigmoid_input *inputs, int input_length) {
+	float sum = 0.0f; 
+
+	for(int i = 0; i < input_length; i++) {
+		sum = sum + (inputs[i].value * inputs[i].weight);
+	}
+	return sum;
+}
+
+
+bool fire_percpetron(struct perceptron_input *inputs, int threshold, int input_length){ 
 	for(int i = 0; i < input_length; i++) {
 		printf("input value: %d, weight: %d \n", inputs[i].value, inputs[i].weight);
 	}
 
-	int weight_sum = sum_of_weights(inputs, input_length); 
+	int weight_sum = sum_of_perceptron_weights(inputs, input_length); 
 
 	printf("sum of weights is: %d \n", weight_sum);
 	printf("Threshhold is: %d \n \n" , threshold);
 
-	int ret; 
 
-	if(weight_sum > threshold) {
-		ret = 1; 
-	} else {
-		ret = 0; 
-	}
-
-	return ret;
+	return weight_sum - threshold <= 0;
 }
 
-int sum_of_weights(struct neuron_input *inputs, int input_length) {
+int sum_of_perceptron_weights(struct perceptron_input *inputs, int input_length) {
 	int sum = 0; 
 
 	for(int i = 0; i < input_length; i++) {
